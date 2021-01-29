d6c18beecd44b8b921e0786f4b514ade7fe82964
Rmolimock <495@holbertonschool.com>
2021-01-29 12:59:21 -0800

    using mongodb built in method for counting docs

--- utils/db.js
+++ utils/db.js
@@ -4,66 +4,44 @@ const assert = require('assert');
 
 
 class DBClient {
-  constructor() {
-    let host = 'localhost';
-    let port = "27017";
-    let dbName = "files_manager";
-    if (process.env.DB_HOST) {
-      let host = process.env.DB_HOST;
-    }
-    if (process.env.DB_PORT) {
-	let post = process.env.DB_PORT;
-    }
-    if (process.env.DB_DATABASE) {
-	let dbName = process.env.DB_DATABASE;
-    }
-    const url = 'mongodb://' + host + ':' + port;  
-    MongoClient.connect(url, function(err, client) { 
-      DBClient.connection = false;
-      if (assert.equal(null, err)) {
-        DBClient.db = client.db(dbName);  
-        DBClient.connection = true;
+
+    constructor() {
+      let host = 'localhost';
+      let port = "27017";
+      let dbName = "files_manager";
+      let db = null;
+      if (process.env.DB_HOST) {
+        let host = process.env.DB_HOST;
       }
-    });
-  }
+      if (process.env.DB_PORT) {
+  	let post = process.env.DB_PORT;
+      }
+      if (process.env.DB_DATABASE) {
+	let dbName = process.env.DB_DATABASE;
+      }
+      const url = 'mongodb://' + host + ':' + port;  
+      MongoClient.connect(url, function(err, client) { 
+        if (assert.equal(null, err)) {
+          this.db = client.db(dbName);
+	  this.db.createCollection('users');
+          this.db.createCollection('files');
+        }
+        if (err) console.log(err);
+      });
+    }
 
-  callback(ar) {
-   return ar.length();
-  }
-
-  isAlive() {
-	  return DBclient.connection;
-  }
-
+    isAlive() {
+      if (this.db) {
+        return true;
+      }
+      return false;
+    }
 
 
-async nbUsers() {
- let db = DBClient.db;
- let callback = DBClient.callback;
- await function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('users');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      return callback(docs);
-    });
-  }
-}
+    async nbUsers() { return this.db.collection('users').countDocuments(); }
 
-async nbFiles() {
-	let db = DBClient.db;
-  let callback = DBClient.callback;
- await function(db, callback) {                                                                                            
-    // Get the documents collection                                                                                             
-    const collection = db.collection('files');                                                                             
-    // Find some documents                                                                                                      
-    collection.find({}).toArray(function(err, docs) {                                                                           
-      assert.equal(err, null);                                                                                                  
-      return callback(docs);                                                                                                    
-    });                                                                                                                         
-  }                                                                                                                             
-} 
+    async nbFiles() { return this.db.collection('files').countDocuments(); }
+
 
 }
 
