7db0fd1321ecea84bf185efd1a52be1da666153f
Rmolimock <495@holbertonschool.com>
2021-01-29 13:38:59 -0800

    simplified isAlive and now awaiting nmUsers and nmFiles

--- utils/db.js
+++ utils/db.js
@@ -1,38 +1,42 @@
 const MongoClient = require('mongodb').MongoClient;                                                                             
-const assert = require('assert');                                                                                               
 
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const dbName = process.env.DB_DATABASE || 'files_manager'; 
 
 
 class DBClient {
-      const host = process.env.DB_HOST || 'localhost';
-      const port = process.env.DB_PORT || 27017;
-      const dbName = process.env.DB_DATABASE || 'files_manager'; 
-      let db = null;
-      const url = 'mongodb://' + host + ':' + port;  
-      MongoClient.connect(url, function(err, client) { 
-        if (err) console.log(err);
-        this.db = client.db(dbName);
-	this.db.createCollection('users');
-        this.db.createCollection('files');
-      });
+    constructor() {
+      this.db = null;
+      const url = "mongodb://" + host + ":" + port + "/" + dbName
+      MongoClient.connect(
+        url,
+        { useUnifiedTopology: true },
+        (err, client) => {
+          if (err) this.db = false;
+          else {
+            this.db = client.db(dbName);
+            this.db.createCollection('users');
+            this.db.createCollection('files');
+          }
+        },
+      );
     }
-
+ 
     isAlive() {
-      if (this.db) {
-        return true;
-      }
-      return false;
+      return !!this.client;
     }
 
 
     async nbUsers() {
-      return this.db.collection('users').countDocuments();
+      const numUsers = await this.client.collection('users').estimatedDocumentCount({});
+      return numUsers;
     }
 
     async nbFiles() {
-      return this.db.collection('files').countDocuments();
+      const numFiles = await this.client.collection('files').estimatedDocumentCount({});
+      return numFiles;
     }
-
 
 }
 
